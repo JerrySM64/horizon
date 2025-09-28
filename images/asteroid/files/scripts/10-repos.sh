95ece2dbda7e93bc822de08d9fd9c517141558ff
Alxhr0 <alxhr0@proton.me>
2025-09-29 00:42:55 +0200

    Test Asteroid

---
+++ images/asteroid/files/scripts/10-repos.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+dnf5 install -y wget dnf5-plugins
+
+# Enable RPMFusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
+
+
+# Enable home_Alxhr0
+dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:Alxhr0/Fedora_42/home:Alxhr0.repo
+
+# Eza
+dnf copr -y enable alternateved/eza 
+
+# Nushell
+echo "[gemfury-nushell]
+name=Gemfury Nushell Repo
+baseurl=https://yum.fury.io/nushell/
+enabled=1
+gpgcheck=0
+gpgkey=https://yum.fury.io/nushell/gpg.key" | tee /etc/yum.repos.d/fury-nushell.repo
+
+# Enable the TKG kernel
+dnf copr -y enable whitehara/kernel-tkg-zen2
+
+# Enable Steam repo
+dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo &&
