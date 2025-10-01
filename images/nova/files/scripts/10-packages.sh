a2eef32123e9a0630e5ccdc274dffc0f6051b5e5
Alxhr0 <alxhr0@proton.me>
2025-10-02 00:43:01 +0200

    Try to resolve VLC conflict x2

--- images/nova/files/scripts/10-packages.sh
+++ images/nova/files/scripts/10-packages.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Install packages
-dnf5 -y install vlc-plugins-freeworld
+dnf5 -y remove libva-intel-media-driver vlc*
 dnf5 -y install --allowerasing @cosmic-desktop-environment flatpak onefetch fastfetch java-21-openjdk-devel libadwaita-devel pkgconf neovim eza bat starship hourglass btop git virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer libguestfs-tools python3-libguestfs virt-top edk2-ovmf swtpm google-noto-fonts-all
 dnf5 -y install --setopt=install_weak_deps=False steam gamemode
 
