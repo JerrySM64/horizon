c984a4d7858522aab60f8395896e6ee3d2f0c28f
Alxhr0 <alxhr0@proton.me>
2025-10-02 00:51:52 +0200

    Annoying VLC.

--- images/nova/files/scripts/10-packages.sh
+++ images/nova/files/scripts/10-packages.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # Install packages
 dnf5 -y remove libva-intel-media-driver
-dnf5 -y install --exclude vlc-plugins-base pipewire-codec-aptx --allowerasing @cosmic-desktop-environment flatpak onefetch fastfetch java-21-openjdk-devel libadwaita-devel pkgconf neovim eza bat starship hourglass btop git virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer libguestfs-tools python3-libguestfs virt-top edk2-ovmf swtpm google-noto-fonts-all
+dnf5 -y install --allowerasing @cosmic-desktop-environment flatpak onefetch fastfetch java-21-openjdk-devel libadwaita-devel pkgconf neovim eza bat starship hourglass btop git virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer libguestfs-tools python3-libguestfs virt-top edk2-ovmf swtpm google-noto-fonts-all --exclude vlc-plugins-base pipewire-codec-aptx
 dnf5 -y install --setopt=install_weak_deps=False steam gamemode
 
 # Replace Kernel
